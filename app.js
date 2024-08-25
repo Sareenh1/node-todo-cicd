8569d81cd97c2a6514c0fa00ab6c4cb2f6b2a114
H Sareen Kumar <sareenh10@gmail.com>
2024-08-25 21:13:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     // In order to use PUT HTTP verb to edit item
     methodOverride = require('method-override'),
-    // Mitigate XSS using sanitizer
     sanitizer = require('sanitizer'),
     app = express(),
     port = 8000
